9c0a1a5d8b79dd89ae2ef61c6bd7ed6f171ba000
gulanxiu <gulanxiu@outlook.com>
2018-11-11 23:01:06 +0800

    D同学

--- counter.js
+++ counter.js
@@ -1,16 +1,37 @@
 
 var c=0
 var t
+
 function timedCount()
 {
-document.getElementById('txt').value=c
+document.getElementById('txt').value="停止计时!"
+document.getElementById("time").value=c
 c=c+1
 t=setTimeout("timedCount()",1000)
 }
 
+
 function stopCount()
 {
-c=0;
-setTimeout("document.getElementById('txt').value=0",0);
+    c=0
 clearTimeout(t);
+document.getElementById('txt').value="开始计时!"
+document.getElementById("time").value=0
+}
+
+var sign=false
+
+function controller()
+{
+    if(sign==false)
+    {
+        sign=true
+        timedCount()
+    }
+    else
+    {
+        sign=false
+        stopCount()
+        
+    }
 }
